470a6c55352853360b79ae0a5b409a5348b205f0
Rasmus Nordström <nordstrom.rasmus@gmail.com>
2015-06-29 21:15:39 +0200

    Added some API class files.

---
+++ API/anforanden.py
@@ -0,0 +1,8 @@
+import logging
+
+class Anforanden(object):
+    '''Holds information classed as "Anforanden"'''
+    def __init__(self):
+        raise NotImplementedError
+
+
---
+++ API/dokument.py
@@ -0,0 +1,9 @@
+
+
+class Dokument(object):
+    '''This class handles all infomation classed
+    as "Dokuemnt"'''
+    def __init__(self):
+        raise NotImplementedError
+
+
