d47aec39641c61c8544ff39492f0722897b226ed
Rasmus Nordström <nordstrom.rasmus@gmail.com>
2015-06-30 18:39:08 +0200

    Rensade lite i koden samt bytte ut **kwargs mot förbestämda keyword-arguments.

--- riksdagen/ledamoter.py
+++ riksdagen/ledamoter.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+"""
+Denna modul hanterar data kring statsapparatens ledamöter.
+"""
+
 import logging
 import sys
 import requests
 import bs4
 import json
 
-class Ledamoter(object):
-    '''Handles information classed as "Ledamoter"'''
-    _ledamoter = []
-    def __init__(self):
-        raise NotImplementedError
-
-
-def _get_search_url(**kwargs):
+def _get_search_url(fornamn, efternamn, fodelsear, kon, parti, valkrets):
     """"""
-    url = ('http://data.riksdagen.se/personlista/?iid=&fnamn=' + 
-           kwargs.get('fornamn', '') + '&enamn=' + 
-           kwargs.get('efternamn', '') +
-           '&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat=json&termlista=')
+    url = ('http://data.riksdagen.se/personlista/?iid='+
+           '&fnamn=' + fornamn  + '&enamn=' + efternamn + 
+           '&f_ar=' + fodelsear + '&kn=' + kon +
+           '&parti=' + parti    + '&valkrets=' + valkrets +
+           '&rdlstatus=&org=&utformat=json&termlista=')
     
     return url
     
-def findall(**kwargs):
+def findall(fornamn = '', efternamn = '', fodelsear = '', kon = '', 
+            parti = '', valkrets = ''):
     """Returnerar en lista innehållandes
     alla träffar.
     
     todo: Byt ut **kwargs mot alla tänkbara sökval."""
     
     #Generate the search url
-    url = _get_search_url(**kwargs)
+    url = _get_search_url(fornamn, efternamn, fodelsear, 
+                          kon, parti, valkrets)
     
     #Fetch the data
     response = requests.get(url)
@@ -43,7 +43,7 @@ def findall(**kwargs):
         
 def _test():
     
-    for skurk in findall(fornamn = "Erik"):
+    for skurk in findall(kon = "man", parti = 'FP'):
         print("Id      : %s\n"  % skurk['hangar_guid'],
               "Fornamnn: %s\n"  % skurk['tilltalsnamn'],
               "Efternamn: %s\n" % skurk['efternamn'],
